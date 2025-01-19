cf1c25f2369bf16a42e42d8713b284036b2e7af7
DeM4rcio <henriquemarcio170@gmail.com>
2025-01-19 11:19:29 -0300

    versão mais otimizada para o probleam 757

--- 757.SetIntersectionSizeAtLeastTwo.py
+++ 757.SetIntersectionSizeAtLeastTwo.py
@@ -1,26 +1,23 @@
+from typing import List
 
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda x:x[1])
+        
+        intervals.sort(key=lambda x: x[1])
+        
         size = 0
-        prev_start = -1
-        prev_end = -1
-
+        prev_points = [] 
+        
         for curr_start, curr_end in intervals:
-            if prev_start == -1 or prev_end < curr_start: #if intervals do not overlap
+           
+            if len(prev_points) < 2 or prev_points[-2] < curr_start:
+                
+                prev_points.append(curr_end - 1)
+                prev_points.append(curr_end)
                 size += 2
-                prev_start = curr_end-1
-                prev_end = curr_end
-
-            elif prev_start < curr_start: #if intervals overlap
-                if prev_end != curr_end:
-                    prev_start = prev_end
-                    prev_end = curr_end
-                    
-                else:
-                    prev_start = curr_end-1
-                    prev_end = curr_end
-
+            elif prev_points[-1] < curr_start:
+                
+                prev_points.append(curr_end)
                 size += 1
 
         return size
