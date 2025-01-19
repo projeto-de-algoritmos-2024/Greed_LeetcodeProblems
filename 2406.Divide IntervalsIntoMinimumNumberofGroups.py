f3083bd617aacdadae309e0b52a9fd55fa668ea6
DeM4rcio <henriquemarcio170@gmail.com>
2025-01-18 23:41:32 -0300

    versão completa desafio 2406

--- 2406.Divide IntervalsIntoMinimumNumberofGroups.py
+++ 2406.Divide IntervalsIntoMinimumNumberofGroups.py
@@ -5,7 +5,7 @@ class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
         events = defaultdict(int)
         
-      
+    
         for start, end in intervals:
             events[start] += 1  
             events[end + 1] -= 1  
@@ -13,6 +13,8 @@ class Solution:
        
         max_groups = 0
         current_groups = 0
-       
+        for time in sorted(events.keys()):
+            current_groups += events[time]
+            max_groups = max(max_groups, current_groups)
         
         return max_groups
